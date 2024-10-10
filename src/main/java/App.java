50228fce68a9ffab0c1a0b7f6f5fd183952fc722
Arman <maanukyanarman@gmail.com>
2024-10-10 18:00:46 +0400

    beans

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorld1 = applicationContext.getBean("helloWorld", HelloWorld.class);
+        HelloWorld helloWorld2 = applicationContext.getBean("helloWorld", HelloWorld.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(cat1 == cat2);
+    }
+}
---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "helloWorld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getTom() {
+        Cat cat = new Cat();
+        cat.setName("Tom");
+        return cat;
+    }
+}
